8d4d8263ffefa788ccdf69c3e748d08c5ee59e17
Koorosh Roodbaraky <koorosh.roodbaraky@mhr.co.uk>
2024-12-16 16:57:09 +0000

    Fixed line scaling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  60,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   2000
@@ -30,9 +30,9 @@ class Ball {
     this.dy = (Math.random() * this.dmax - 2) * 0.1;
     this.dz = (Math.random() * this.dmax - 2) * 0.1;
     this.ball.position.set(
-        Math.random() * bounds - bounds / 2,
-        Math.random() * bounds - bounds / 2,
-        Math.random() * bounds - bounds / 2
+      Math.random() * bounds - bounds / 2,
+      Math.random() * bounds - bounds / 2,
+      Math.random() * bounds - bounds / 2
     );
   }
 
@@ -46,28 +46,29 @@ class Ball {
   }
 }
 
-const balls = Array.from({ length: 10 }, () => new Ball());
+const balls = Array.from({ length: 20 }, () => new Ball());
 balls.forEach((ball) => scene.add(ball.ball));
 
 camera.position.z = bounds * 3;
+
 function drawLine(ball1, ball2) {
   const dist = ball1.ball.position.distanceTo(ball2.ball.position);
-  const material = new THREE.LineBasicMaterial({
-    color: 0xffffff,
-    transparent: true,
-    opacity: `${2 / dist}`,
-    linewidth: `${10 / dist}`,
-  });
-  const points = [];
-  if (dist < 10) {
+  if (dist < bounds / 0.7) {
+    const material = new THREE.LineBasicMaterial({
+      color: 0xffffff,
+      transparent: true,
+      opacity: Math.min(1, 2 / dist),
+      linewidth: Math.min(1, 10 / dist),
+    });
+    const points = [];
     points.push(ball1.ball.position.clone());
     points.push(ball2.ball.position.clone());
-  }
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  const line = new THREE.Line(geometry, material);
-  scene.add(line);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    scene.add(line);
 
-  setTimeout(() => scene.remove(line), 16);
+    setTimeout(() => scene.remove(line), 10);
+  }
 }
 
 function animate() {
@@ -83,11 +84,11 @@ function animate() {
 }
 
 window.addEventListener("resize", () => {
-    const bounds = Math.min(window.innerWidth, window.innerHeight) / 100;
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.position.z = bounds * 3; 
-  });
-  
+  const bounds = Math.min(window.innerWidth, window.innerHeight) / 100;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.z = bounds * 3;
+});
+
 renderer.setAnimationLoop(animate);
